43364f1995522eba2fb15f85665d40dc4cf710e0
kaustubh lakhan <kaustubhlakhan@gmail.com>
2025-12-17 00:37:26 +0530

    second commit

--- Terraform/aws assignment/variables.tf
+++ Terraform/aws assignment/variables.tf
@@ -13,5 +13,5 @@ variable "domain_name" {
 variable "ecr_repository_url" {
   description = "The ECR repository URL for the microservice image (e.g., 123456789012.dkr.ecr.us-east-1.amazonaws.com/my-repo)"
   type        = string
-  default     = "public.ecr.aws/f8k5n6s7/sample-microservice" # Provide a default or placeholder
+  default     = "546454332581.dkr.ecr.us-east-1.amazonaws.com/kaustubh/microservice-repo" # Provide a default or placeholder
 }
